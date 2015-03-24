d34bb4905a61f491726b88084ba4f5e5465d6917
kristovanoha <jiri.hegar@centrum.cz>
2015-03-24 14:28:06 +0100

    entity s tabulkou

--- EntityFrameworkTEST/MainWindow.xaml.cs
+++ EntityFrameworkTEST/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,30 @@ namespace EntityFrameworkTEST
         {
             InitializeComponent();
         }
+
+        private DatabaseSlovnikEntities _context = new DatabaseSlovnikEntities();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Data.CollectionViewSource testViewSource =
+                ((System.Windows.Data.CollectionViewSource)(this.FindResource("testViewSource")));
+            
+            _context.Test.Load();
+            testViewSource.Source = _context.Test.Local;
+
+
+            //_context.Test.Load();
+            //// Load data by setting the CollectionViewSource.Source property:
+            //englishViewSource.Source = _context.Test.Local;
+            ////gridTest.DataContext = _context.Test;
+            ////gridTest.
+
+            //var a = 4;
+            //System.Windows.Data.CollectionViewSource testViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("testViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // testViewSource.Source = [generic data source]
+        }
     }
+
+
 }

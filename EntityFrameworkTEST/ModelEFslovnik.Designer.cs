dd0463959dc80c437b02968b41ae0ac1f50c028d
kristovanoha <jiri.hegar@centrum.cz>
2015-03-25 07:57:35 +0100

    last

--- EntityFrameworkTEST/ModelEFslovnik.Designer.cs
+++ EntityFrameworkTEST/ModelEFslovnik.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\hegarj\documents\visual studio 2013\Projects\EntityFrameworkTEST\EntityFrameworkTEST\ModelEFslovnik.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hegarj\Documents\Visual Studio 2013\Projects\EntityFrameworkTEST\EntityFrameworkTEST\ModelEFslovnik.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
